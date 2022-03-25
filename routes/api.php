88731290a5d88738f474536ff2a9486b184a946b
SergiCB99 <sergicampanalesbalana@gmail.com>
2022-03-25 19:36:34 +0100

    Tasca 4
    Funcionalitat Crear BD Historial

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('history/{package}',[\App\Http\Controllers\ApiHistoryController::class,'show'])->name('history_installs');
