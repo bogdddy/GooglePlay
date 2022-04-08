b4e9964e493d64accd2ef76f4077180e4fd9d877
Jordi <jorditml1@gmail.com>
2022-04-08 20:23:15 +0200

    Fixed show view of apps

--- resources/views/app/show.blade.php
+++ resources/views/app/show.blade.php
@@ -31,148 +31,154 @@
 		<div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
 			
 			<!--Console Content-->
-			<div class="grid grid-rows-3 grid-flow-col auto-cols-max gap-4">
-                <div class="row-span-3 w-full  md:h-1/4  p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                @foreach($app->images as $image)
-                                    @if ($image->type_id == 1)
-                                        <img src="{{$image->url}}">
-                                    @endif
-                                @endforeach
-                             </div></div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Nom</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">{{$app->name}} </h3>
-                            </div>
-                     </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="col-span-2 w-full md:h-2/4 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Develeoper</h5>
-                                <h3 class="font-bold text-1xl text-gray-600">{{$app->developer->name}} </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="col-span-2 w-full md:h-2/4 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Category</h5>
-                                <h3 class="font-bold text-1xl text-gray-600">{{$app->category->name}} </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-            </div>
-            <!-- Separació -->
-            <hr class="border-b-2 border-gray-600 my-8 mx-4">
-            <div class="flex flex-wrap">
-                <div class="w-full md:w-1/2 xl:w-1/5 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">ID GooglePlay</h5>
-                                <h3 class="font-bold text-1xl text-gray-600">{{$app->google_play_id}} <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/6 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">País</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">{{$app->country}} <span class="text-yellow-600"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/4 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Link</h5>
-                                <h3 class="font-bold text-1xl text-gray-600">{{$app->url}}</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/6 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Score</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">{{$app->score}}</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/5 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-red-600"><i class="fas fa-inbox fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-400">Issues</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">3 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-            </div>
+			<div class="grid grid-rows-1 grid-flow-col auto-cols-max gap-4">
 
-			<!--Divider-->
-			<hr class="border-b-2 border-gray-600 my-8 mx-4">
+                <div class="row-span-3 w-80 pt-20 md:h-1/8  p-0">
+                    <!--Metric Card-->
+
+                    <div class="pr-0">
+                        @foreach($app->images as $image)
+                            @if ($image->type_id == 1)
+                                <img class="rounded-lg" src="{{$image->url}}">
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+                <div class="grid grid-cols-3 px-16 gap-2 text-right md:text-center">
+                    <div class="md:h-1/4 p-1 col-span-3">
+                        <h6 class="font-bold uppercase text-gray-600">&nbsp;</h6>
+                        <h3 class="font-bold text-3xl text-gray-1000">{{$app->name}} </h3>
+                    </div>      
+                   
+            
+                    <div class="w-full md:h-1/4 p-1 col-span-2" >
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+
+                                <div class="flex-1 text-right md:text-center">
+                                    <h3 class="font-bold text-1xl text-gray-600">Develeoper: {{$app->developer->name}} </h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+
+                    <div class=" w-full md:h-1/4 p-1">
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-1 text-right md:text-center">
+                                    <h3 class="font-bold text-1xl text-gray-600">Category: {{$app->category->name}} </h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+                    <hr class="border-b-2 border-gray-600 my-8 mx-4 col-span-3">
+                    <div class=" w-full md:h-1/4 p-1">
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-shrink pr-4">
+                                    <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
+                                </div>
+                                <div class="flex-1 text-right md:text-center">
+                                    <h5 class="font-bold uppercase text-gray-800">ID GooglePlay</h5>
+                                    <h3 class="font-bold text-1xl text-gray-600">{{$app->google_play_id}} <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+                    <div class=" w-full md:h-1/4 p-1">
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-shrink pr-4">
+                                    <div class="rounded p-3 bg-red-600"><i class="fas fa-inbox fa-2x fa-fw fa-inverse"></i></div>
+                                </div>
+                                <div class="flex-1 text-right md:text-center">
+                                    <h5 class="font-bold uppercase text-gray-800">País</h5>
+                                    <h3 class="font-bold text-1xl text-gray-600">{{$app->country}}  <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+
+                    <div class=" w-full md:h-1/4 p-1">
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-shrink pr-4">
+                                    <div class="rounded p-3 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i></div>
+                                </div>
+                                <div class="flex-1 text-right md:text-center">
+                                    <h5 class="font-bold uppercase text-gray-800">Score</h5>
+                                    <h3 class="font-bold text-1xl text-gray-600">{{$app->score}}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+
+                    <div class=" w-full md:h-1/4 p-1 col-span-2"> 
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-shrink pr-4">
+                                    <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
+                                </div>
+                                <div class="flex-1 text-right md:text-center">
+                                    <h5 class="font-bold uppercase text-gray-800">Link</h5>
+                                    <h3 class="font-bold text-1xl text-gray-600">{{$app->url}}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+
+                    <div class=" md:h-1/4 p-1"> 
+                    <!--Metric Card-->
+                        <div class="bg-gray-300 border border-gray-600 rounded shadow p-2">
+                            <div class="flex flex-row items-center">
+                                <div class="flex-shrink pr-4">
+                                    <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
+                                </div>
+                                <div class="flex-1 text-right md:text-center">
+                                    <h5 class="font-bold uppercase text-gray-800">Installs</h5>
+                                    <h3 class="font-bold text-1xl text-gray-600">{{$app->installs}}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/Metric Card-->
+                    </div>
+                                  
+                </div> 
+                                    
+            </div>
+     
+                    <!--Metric Card-->
+                    <div class="pt-12 ">
+                        <hr class="border-b-2 border-gray-600 w-full"> 
+                    </div>   
+                        
+                              
+                                    <h5 class="font-bold uppercase pt-12 text-gray-800">Descripció</h5>
+                                    <div class="font-bold text-gray-600 whitespace-pre-line w-96">{{$app->description}}</div>
+                           
+         
 
             
-								
-			<!--/ Console Content-->
 					
 		</div>
+
+       
 		
 
 	</div> 
-	
+
 
 
 @endsection
