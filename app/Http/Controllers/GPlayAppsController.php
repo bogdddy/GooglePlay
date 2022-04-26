27a21cddb860fdc27f5228c1c6f54f4d66cc3b91
SergiCB99 <sergicampanalesbalana@gmail.com>
2022-04-26 17:52:11 +0200

    Code review Tasca 3

--- app/Http/Controllers/GPlayAppsController.php
+++ app/Http/Controllers/GPlayAppsController.php
@@ -12,9 +12,14 @@ use App\Models\Package;
 use App\Models\Rating;
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Nelexa\GPlay\Model\AppInfo;
 
 class GPlayAppsController extends Controller
 {
+
+    //Limit de packages que agafa per consulta
+    private $LIMIT_COUNT_APPS = 10;
+
     /**
      * getAppsInfo
      * Obtenim info actualitzada de les apps de google play
@@ -24,12 +29,45 @@ class GPlayAppsController extends Controller
     public function getAppsInfo()
     {
         //* Obtenim les id de les apps de la database
-        $appsDB = Package::orderBy('updated_at', 'asc')->limit(10)->get();
+        $appsDB = Package::orderBy('updated_at', 'asc')->limit(GPlayAppsController::$LIMIT_COUNT_APPS)->get();
 
         foreach ($appsDB as $appDB) {
             //* obtenim les dades de google play
             $gplay = new \Nelexa\GPlay\GPlayApps($defaultLocale = 'ca_ES', $defaultCountry = 'es');
             $appInfo = $gplay->getAppInfo($appDB->app_url);
+
+            //Hem tret la següent part en funcions més petites ja que era una funcio massa llarga
+            $category = insertarActualitzarCategory($appInfo);
+
+            $developer = insertarActualitzarDepeloper($appInfo);
+
+            $app = insertarActualitzarApp($appInfo,$category,$developer);
+
+            insertarActualitzarRating($appInfo,$app);
+
+            insertarActualitzarVideo($appInfo,$app);
+
+            insertarActualitzarIcon($appInfo,$app);
+
+            insertarActualitzarCover($appInfo,$app);
+
+            insertarActualitzarScreenshot($appInfo,$app);
+
+            History::updateHistory($appDB->app_url,$appInfo->getInstalls());
+
+            $appDB->touch();
+        }
+
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Categories
+     * @param AppInfo $appInfo
+     * @return Category $category
+     */
+    private function insertarActualitzarCategory (AppInfo $appInfo)
+    {
 
             $categoryInfo = $appInfo->getCategory();
             $category = Category::updateOrCreate(
@@ -44,110 +82,180 @@ class GPlayAppsController extends Controller
                 ]
             );
 
-            $developerInfo = $appInfo->getDeveloper();
-            $developer = Developer::updateOrCreate(
-                [
-                    "google_play_id" => $developerInfo->getId()
-                ],
-                [
-                    "name" => $developerInfo->getName(),
-                    "url" => $developerInfo->getUrl(),
-                    "website" => $developerInfo->getWebsite()
-                ]
-            );
+            return $category;
 
-            $app = App::updateOrCreate(
-                [
-                    "google_play_id" => $appInfo->getId()
-                ],
-                [
-                    "name" => $appInfo->getName(),
-                    "country" => $appInfo->getCountry(),
-                    "url" => $appInfo->getUrl(),
-                    "score" => $appInfo->getScore(),
-                    "installs" => $appInfo->getInstalls(),
-                    "price" => $appInfo->getPrice(),
-                    "description" => $appInfo->getDescription(),
-                    "release_date" => $appInfo->getReleased(),
-                    "category_id" => $category->id,
-                    "developer_id" => $developer->id,
-                    "update_date" => $appInfo->getUpdated()
-                ]
-            );
+    }
 
-            $ratingInfo = $appInfo->getHistogramRating();
-            $app->rating()->updateOrCreate(
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Developer
+     * @param AppInfo $appInfo
+     * @return Developer $developer
+     */
+    private function insertarActualitzarDeveloper (AppInfo $appInfo){
+
+        $developerInfo = $appInfo->getDeveloper();
+        $developer = Developer::updateOrCreate(
+            [
+                "google_play_id" => $developerInfo->getId()
+            ],
+            [
+                "name" => $developerInfo->getName(),
+                "url" => $developerInfo->getUrl(),
+                "website" => $developerInfo->getWebsite()
+            ]
+        );
+
+        return $developer;
+
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Apps
+     * @param AppInfo $appInfo
+     * @param Category $category
+     * @param Developer $developer
+     * @return App $app
+     */
+    private function insertarActualitzarApp(AppInfo $appInfo,Category $category,Developer $developer){
+
+        $app = App::updateOrCreate(
+            [
+                "google_play_id" => $appInfo->getId()
+            ],
+            [
+                "name" => $appInfo->getName(),
+                "country" => $appInfo->getCountry(),
+                "url" => $appInfo->getUrl(),
+                "score" => $appInfo->getScore(),
+                "installs" => $appInfo->getInstalls(),
+                "price" => $appInfo->getPrice(),
+                "description" => $appInfo->getDescription(),
+                "release_date" => $appInfo->getReleased(),
+                "category_id" => $category->id,
+                "developer_id" => $developer->id,
+                "update_date" => $appInfo->getUpdated()
+            ]
+        );
+
+        return $app;
+
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Apps
+     * @param AppInfo $appInfo
+     * @param App $app
+     */
+    private function insertarActualitzarRating(AppInfo $appInfo,App $app){
+
+        $ratingInfo = $appInfo->getHistogramRating();
+        $app->rating()->updateOrCreate(
+            [
+                "app_id" => $app->id
+            ],
+            [
+                "five_stars" => $ratingInfo->getFiveStars(),
+                "four_stars" => $ratingInfo->getFourStars(),
+                "three_stars" => $ratingInfo->getThreeStars(),
+                "two_stars" => $ratingInfo->getTwoStars(),
+                "one_star" => $ratingInfo->getOneStar()
+            ]
+        );
+
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Videos
+     * @param AppInfo $appInfo
+     * @param App $app
+     */
+    private function insertarActualitzarVideo(AppInfo $appInfo,App $app){
+
+        $videoInfo = $appInfo->getVideo();
+        if (isset($videoInfo)) {
+            Video::updateOrCreate(
                 [
                     "app_id" => $app->id
                 ],
                 [
-                    "five_stars" => $ratingInfo->getFiveStars(),
-                    "four_stars" => $ratingInfo->getFourStars(),
-                    "three_stars" => $ratingInfo->getThreeStars(),
-                    "two_stars" => $ratingInfo->getTwoStars(),
-                    "one_star" => $ratingInfo->getOneStar()
+                    'image_url' => $videoInfo->getImageUrl(),
+                    'youtube_id' => $videoInfo->getYoutubeId(),
+                    'url' => $videoInfo->getVideoUrl()
                 ]
             );
-
-            $videoInfo = $appInfo->getVideo();
-            if (isset($videoInfo)) {
-                Video::updateOrCreate(
-                    [
-                        "app_id" => $app->id
-                    ],
-                    [
-                        'image_url' => $videoInfo->getImageUrl(),
-                        'youtube_id' => $videoInfo->getYoutubeId(),
-                        'url' => $videoInfo->getVideoUrl()
-                    ]
-                );
-            }
-            $icon = $appInfo->getIcon();
-
-            if (isset($icon)) {
-                Image::updateOrCreate(
-                    [
-                        "app_id" => $app->id,
-                        "type_id" => 1
-                    ],
-                    [
-                        "url" => $icon->getUrl()
-                    ]
-                );
-
-            }
-            $cover = $appInfo->getCover();
-            if (isset($cover)) {
-                Image::updateOrCreate(
-                    [
-                        "app_id" => $app->id,
-                        "type_id" => 2
-                    ],
-                    [
-                        "url" => $cover->getUrl()
-                    ]
-                );
-            }
-
-            $screenshots = $appInfo->getScreenshots();
-            if (count($screenshots) > 0) {
-                foreach ($screenshots as $screenshot) {
-                    Image::updateOrCreate(
-                        [
-                            "app_id" => $app->id,
-                            "type_id" => 3
-                        ],
-                        [
-                            "url" => $screenshot->getUrl()
-                        ]
-                    );
-                }
-            }
-
-            History::updateHistory($appDB->app_url,$appInfo->getInstalls());
-
-            $appDB->touch();
         }
 
     }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Images
+     * @param AppInfo $appInfo
+     * @param App $app
+     */
+    private function insertarActualitzaIcon(AppInfo $appInfo,App $app){
+        $icon = $appInfo->getIcon();
+
+        if (isset($icon)) {
+            Image::updateOrCreate(
+                [
+                    "app_id" => $app->id,
+                    "type_id" => 1
+                ],
+                [
+                    "url" => $icon->getUrl()
+                ]
+            );
+
+        }
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Images
+     * @param AppInfo $appInfo
+     * @param App $app
+     */
+    private function insertarActualitzarCover(AppInfo $appInfo,App $app){
+        $cover = $appInfo->getCover();
+        if (isset($cover)) {
+            Image::updateOrCreate(
+                [
+                    "app_id" => $app->id,
+                    "type_id" => 2
+                ],
+                [
+                    "url" => $cover->getUrl()
+                ]
+            );
+        }
+    }
+
+    /**
+     * Inserta o actualitza en cas que ja existeixi
+     * a la taula Images
+     * @param AppInfo $appInfo
+     * @param App $app
+     */
+    private function insertarActualitzarScreenshoot(AppInfo $appInfo, App $app){
+        $screenshots = $appInfo->getScreenshots();
+        if (count($screenshots) > 0) {
+            foreach ($screenshots as $screenshot) {
+                Image::updateOrCreate(
+                    [
+                        "app_id" => $app->id,
+                        "type_id" => 3
+                    ],
+                    [
+                        "url" => $screenshot->getUrl()
+                    ]
+                );
+            }
+        }
+    }
+
 }
